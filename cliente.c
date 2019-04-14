3c74eb145ee569d6590dacca9ee2130ceb0d11f4
EmersonAS <emersonls2008@hotmail.com>
2019-04-14 12:38:11 -0300

    Verso final do codigo do cliente: Comentado, com troca de msgs iniciais, processando argumentos da linha de comando.

--- cliente.c
+++ cliente.c
@@ -15,9 +15,9 @@
 
 int main(int argc, char const *argv[]) {
 
-	struct sockaddr_in server_addr;
-	int socket_fd;
-	int status = 0;
+	struct sockaddr_in server_addr; 	// Estruturas do tipo endereço de socket
+	int socket_fd;						// Descritor de socket (equivalente a um HANDLE)
+	int status = 0;                     // Para verificar o retorno das funções
 
 	const char *IP_ADDR = argv[1];
 	int PORT = atoi(argv[2]);
@@ -27,113 +27,121 @@ int main(int argc, char const *argv[]) {
 	
 	int BUFFER_SIZE = atoi(argv[4]);
 
-	char buffer[BUFFER_SIZE];
+	char buffer[BUFFER_SIZE];		// buffer para armazenar tempoariamente as msgs enviadas/recebidas
 
-	struct timeval start, end;
+	struct timeval start, end;		// Estruturas com variáveis tv_sec (tipo time_t) e tv_usec (tipo suseconds_t)
 
-    gettimeofday(&start, NULL);
+    gettimeofday(&start, NULL);		// Inicia a contagem de segundos e microsegundos desde 01/01/1970 00:00:00
 
-	FILE * fp;
+    /****************************************************************************
+    * A função socket() retorna um descritor de socket, que representa um       *
+    * ponto final (end point), com base IP4.                                    *
+    ****************************************************************************/
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0); // teste erro -1
+    // Domínio de comunicação: AF_INET (protocolo IPv4)
+    // Tipo de comunicação: SOCK_STREAM: TCP (Reliable, Connection oriented)
+    // Valor do Protocolo para o IP: 0
+
+    printf("Inicializando o cliente.\n");
+
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	
+	if (socket_fd == -1) {
+		printf("Estabelecimento so socket falhou.\n");
+        exit(1);
+	}
+	printf("Socket do cliente criado com sucesso.\n");
+
+	/********************************************************************************************
+	* connect() conecta o socket referenciado por socket_fd ao endereço do servidor server_addr *
+	********************************************************************************************/
+
+	// Preenche os campos da struct server_addr (do tipo socketaddr)
+    // AF_INET: (Protocolo IPv4)
+    // htons: converte variável PORT de host byte order para network byte order
+    // inet_addr(IP_ADDR): converte da notação de números e pontos p/ Notação binária em network byte order
+    // Ex: IP_ADDR = 127.0.0.1 se refere ao host local via loopback device (equivalente a INADDR_LOOPBACK)
+
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(PORT);
 	server_addr.sin_addr.s_addr = inet_addr(IP_ADDR);
 
-	/*
-	IP ADDR OF SERVER HOST:
-
-	There are several special addresses: INADDR_LOOPBACK (127.0.0.1)
-    always refers to the local host via the loopback device; INADDR_ANY
-    (0.0.0.0) means any address for binding; INADDR_BROADCAST
-    (255.255.255.255) means any host and has the same effect on bind as
-    INADDR_ANY for historical reasons.
-	
-	inet_addr: Internet address manipulation routine
-
-	in_addr_t inet_addr(const char *cp);
-
-	The inet_addr() function converts the Internet host address ADDR_SERVER from 
-	IPv4 numbers-and-dots notation into binary data in network byte order. 
-	
-	If the input is invalid, INADDR_NONE (usually -1) is returned. Use of 
-	this function is problematic because -1 is a valid address 
-	(255.255.255.255). Avoid its use in favor of inet_aton(), inet_pton(3), 
-	or getaddrinfo(3) which provide a cleaner way to indicate error return. 
-
-	*/
-
-	// setsockopt(); NO NEED BECAUSE THE SERVER ALREADY RESERVED AND 
-	//            ...IS LISTENNING ON THE PORT WE WANT TO COMMUNICATE
-
-
-	// bind(); NO NEED - WHY??
-
 	socklen_t server_addr_length = sizeof(server_addr);
+	status = connect(socket_fd, (struct sockaddr *) &server_addr, server_addr_length);
 
-	status = connect(socket_fd, (struct sockaddr *) &server_addr, server_addr_length);  // teste erro -1
+	if (status == -1) {
+		printf("Conexão com o servidor falhou.\n");
+        exit(1);
+	}
 
-	status = recv(socket_fd, buffer, BUFFER_SIZE, 0); // receive presentation msg from server // teste erro -1
+    /**********************************************************************************
+	* recv() recebe uma msg de um socket (já conectado), retorna a qtde de bytes      *
+    * recebidos, e os armazena em um buffer de tamanho BUFFER_SIZE.                   *
+    **********************************************************************************/
 
-	int bytes_recv_total = 0;
-	int bytes_recv = 0;
+	status = recv(socket_fd, buffer, BUFFER_SIZE, 0); // Recebe apres. inicial do servidor
+	printf("Servidor: %s", buffer);
+
+	FILE * File_in;				// Ponteiro para o arquivo a ser gravado
+	int bytes_recv_total = 0;   // Inicializa contador do total de bytes gravados no arquivo (recebidos)
+	int bytes_recv = 0;			// Inicializa a contagem de bytes gravados no arquivo
 	
 	if (status != -1) {
-		
-		status = send(socket_fd, FILE_NAME, sizeof(FILE_NAME), 0); // Envia nome do arquivo a ser aberto: string terminada em /0
-		
-		char Name[] = "ClientVersion";
-		fp = fopen(strcat(Name,FILE_NAME), "w+");
+		printf("Conexão estabelecida.\n");
 
-		if(fp == NULL){
-	        printf("Erro na abertura do arquivo. Programa encerrado.\n");
+		/*****************************************************************************************
+    	* send() é usada para transmitir msgs a outro socket. Para ser enviada, a msg é colocada *
+    	* em um buffer de tamanho sizeof(buffer). Retorna a qtde de bytes enviados em caso de    *
+    	* sucesso.														                         *
+    	*****************************************************************************************/
+
+		// Envia nome do arquivo a ser aberto: string terminada em /0
+		status = send(socket_fd, FILE_NAME, sizeof(FILE_NAME), 0);
+		
+		if (status == -1) {
+            printf("Erro ao enviar o nomde do arquivo.\n");
+            exit(1);
+        }
+        printf("Nome do arquivo enviado.\n");
+
+		char Name[] = "ClientVersion";
+		
+		File_in = fopen(strcat(Name,FILE_NAME), "w+");
+
+		if(File_in == NULL){
+	        printf("Erro na abertura do arquivo para gravação.\n");
 	        exit(1);
 	    }
 
-		/*
-		size_t fwrite ( const void * ptr, size_t size, size_t count, FILE * stream );
-			Writes an array of count elements, each one with a size of size bytes, 
-			from the block of memory pointed by ptr to the current position in the stream.
-
-			The position indicator of the stream is advanced by the total number of bytes written.
-
-			Internally, the function interprets the block pointed by ptr as if it was an array of (size*count) 
-			elements of type unsigned char, and writes them sequentially to stream as if fputc was called for each byte.
-		*/
-
 		// Grava dados recebidos no arquivo de saida
 		while( (status = recv(socket_fd, buffer, BUFFER_SIZE, 0)) > 0 ){
-			bytes_recv = fwrite(buffer, sizeof(char), strlen(buffer)/sizeof(char), fp);
-			bytes_recv_total += bytes_recv;
+			
+			if (status == -1) {
+                printf("Erro ao receber dados.\n");
+                exit(1);
+            }
+
+            // Grava no arquivo de saída o conteúdo atual (válido - Até o '\0') de buffer
+			bytes_recv = fwrite(buffer, sizeof(char), strlen(buffer)/sizeof(char), File_in);
+			bytes_recv_total += bytes_recv;		// Atualiza total de bytes recebidos/gravado
 		}
 
 	} else {
-		printf("Conexao falhou. Programa encerrado.\n");
+		printf("Conexão falhou.\n");
 		exit(1);
 	}
 
-	close(socket_fd);	// fecha a conexao
-	fclose(fp);			// fecha arquivo de escrita
+	fclose(File_in);	// Fecha arquivo de escrita
 
-	printf("Conexao fechada.\n");
+	close(socket_fd);	// Fecha a conexão com o socket
+	
+	printf("Conexão fechada.\n");
 
-	gettimeofday(&end,NULL);
+	gettimeofday(&end,NULL);	// Inicia a contagem de segundos e microsegundos desde 01/01/1970 00:00:00
 
-    printf(" %.4f segundos\n %d bytes recebidos\n", 
+	// Tempo gasto é dado pela diferença da última com a primeira contagem (end - start)
+    printf("t = %.4f segundos\nL = %d bytes recebidos\n", 
       ((end.tv_sec + end.tv_usec * 1e-6) - (start.tv_sec + start.tv_usec * 1e-6)), bytes_recv_total);
 	
 	return 0;
 }
-
-/* REFERENCES
-
-1 - inet_addr: 			https://linux.die.net/man/3/inet_addr
-2 - fwrite:				http://www.cplusplus.com/reference/cstdio/fwrite/
-3 - 					https://www.geeksforgeeks.org/readwrite-structure-file-c/
-4 - fwrite strlen:		http://forums.codeguru.com/showthread.php?452056-fwrite()-writes-garbage-at-the-end-of-the-file
-5 - string em C:		https://www.geeksforgeeks.org/strings-in-c-2/
-6 - strlen:				http://www.cplusplus.com/reference/cstring/strlen/
-						https://www.geeksforgeeks.org/strlen-function-in-c/
-
-*/
