679f9fc3abb3303003da328079f51e1fda857b1b
EmersonAS <emersonls2008@hotmail.com>
2019-05-10 01:08:54 -0300

    versão funcional cliente - recebe caracteres digitados do teclado - Criação do Socket e 'conexão' com o servidor corrigidas seguindo modelo do professor

--- TP2/code/servidorFTP.c
+++ TP2/code/servidorFTP.c
@@ -6,49 +6,47 @@
 
 #include "tp_socket.h"
 
-#define BUFFER_INFO_SIZE 1024	// Tamanho máximo do buffer p/ troca de info da conexão e nome do arquivo
+#define FILE_NAME_SIZE 1024	// Tamanho máximo do buffer p/ troca de info da conexão e nome do arquivo
 
 
 typedef struct packet{
-    char data[10];
+    char data[512];
 } Packet;
 
 
-typedef struct frame {
-    int frame_kind;
+typedef struct segment {
+    Packet packet;
     int seq_no;
     int ack;
-    Packet packet;
-} Frame;
+} Segment;
 
 
 int main(int argc, char const *argv[]) {
 
 	int status = 0;                             // Para verificar o retorno das funções
-	int socket_fd;                     			// Descritor de socket (equivalente a um HANDLE)
-    so_addr newAddr;
+	int server_socket;                     	    // Descritor de socket (equivalente a um HANDLE)
+    so_addr client_addr;
 
     int PORT = atoi(argv[1]);
     int BUFFER_DATA_SIZE = atoi(argv[2]);
 
-    char buffer_Info[BUFFER_INFO_SIZE] = {0};	// buffer para...
-    char buffer_Data[BUFFER_DATA_SIZE];     	// buffer para armazenar tempoariamente os bytes (conteúdo) das msgs a serem enviadas
+    char file_Name[FILE_NAME_SIZE] = {0};	// buffer para...
+    
+    char buffer_Data[BUFFER_DATA_SIZE];    // EXCLUIR	// buffer para armazenar tempoariamente os bytes (conteúdo) das msgs a serem enviadas
 
     //struct timeval start, end;    // Estruturas com variáveis tv_sec (tipo time_t) e tv_usec (tipo suseconds_t)
     //gettimeofday(&start, NULL);   // Inicia a contagem de segundos e microsegundos desde 01/01/1970 00:00:00
-
-    status = tp_init();
-    if (status != 0) {
+    
+    if (tp_init() != 0) {
         printf("tp_init falhou.\n");
         exit(1);
     }
 
-    socket_fd = tp_socket((unsigned short) PORT);
-    if (socket_fd < 0) {
+    if ((server_socket = tp_socket((unsigned short) PORT)) < 0) {
         printf("tp_socket falhou.\n");
         exit(1);
     }
-    printf("Socket do servidor criado com sucesso.\n");
+    printf("\tserver socket created.\n");
 
     /*
     FILE * File_read;           // Ponteiro para o arquivo a ser lido
@@ -56,65 +54,76 @@ int main(int argc, char const *argv[]) {
     int bytes_sent = 0;         // Inicializa a contagem de bytes lidos do arquivo
 
     // Recebe o nome do arquivo completo
-	if ((tp_recvfrom(socket_fd, buffer_Info, BUFFER_INFO_SIZE, &newAddr)) < 0) {
+	if ((tp_recvfrom(server_socket, file_Name, FILE_NAME_SIZE, &client_addr)) < 0) {
         printf("File name not received.\n");
         exit(1);
     }
-    printf("File name received: %s\n", buffer_Info);
+    printf("File name received: %s\n", file_Name);
             
-    File_read = fopen(buffer_Info, "r");
+    File_read = fopen(file_Name, "r");
   	if(File_read == NULL){
   	    printf("Can not open file.\n");
   	    exit(1);
   	}
+
+
+        while( (bytes_sent = fread(buffer_Data, sizeof(char), (BUFFER_DATA_SIZE-1), File_read)) > 0 )
+        bytes_sent_total += bytes_sent;             // Atualiza total de bytes lido/enviado
+
     */
 
     
-    status = tp_recvfrom(socket_fd, buffer_Info, BUFFER_INFO_SIZE, &newAddr);
-    printf("%s\n", buffer_Info);
+    status = tp_recvfrom(server_socket, file_Name, FILE_NAME_SIZE, &client_addr);
+    printf("%s\n", file_Name);
 
     
     /* LÓGICA DO STOP-AND-WAIT */
 
-    int frame_id = 0;
-    Frame frame_send;
-    Frame frame_recv;
+    int segment_id = 0;
+    Segment segment_send;
+    Segment segment_recv;
+    
+    //segment_recv.packet.data = malloc(BUFFER_DATA_SIZE);
+    //segment_send.packet.data = malloc(BUFFER_DATA_SIZE);
+
     int ack_recv = 1;
 
     while(1){
         if (ack_recv == 1){
-            frame_send.seq_no = frame_id;
-            frame_send.frame_kind = 1;
-            frame_send.ack = 0;
+            segment_send.seq_no = segment_id;
+            segment_send.ack = 0;
 
             printf("Enter data: ");
             scanf("%s", buffer_Data);
-            strcpy(frame_send.packet.data, buffer_Data);
+            strcpy(segment_send.packet.data, buffer_Data);
 
-            tp_sendto(socket_fd, (char *) &frame_send, sizeof(Frame), &newAddr);
-            printf("Frame sent\n");
+            tp_sendto(server_socket, (char *) &segment_send, sizeof(Segment), &client_addr);
+            printf("Segment sent\n");
         }
         //int addr_size = sizeof(server_addr);
-        int f_recv_size = tp_recvfrom(socket_fd, (char *) &frame_recv, sizeof(Frame), &newAddr);
+        int f_recv_size = tp_recvfrom(server_socket, (char *) &segment_recv, sizeof(Segment), &client_addr);
         
-        if (f_recv_size > 0 && frame_recv.seq_no == 0 && frame_recv.ack == frame_id + 1){
+        if (f_recv_size > 0 && segment_recv.seq_no == 0 && segment_recv.ack == segment_id + 1){
             printf("ACK Received\n");
             ack_recv = 1;
         } else {
             printf("ACK Not Received\n");
             ack_recv = 0;
         }
-        frame_id++;
+        segment_id++;
     }
 
-    /**/
-    //fclose(File_out);   // Fecha arquivo de leitura
+    
+    //fclose(File_read);   // Fecha arquivo de leitura
 
     //gettimeofday(&end, NULL);      // Inicia a contagem de segundos e microsegundos desde 01/01/1970 00:00:00
 
     // Tempo gasto é dado pela diferença da última com a primeira contagem (end - start)
     //printf("t = %.4f segundos\nL = %d bytes enviados\n", 
     //  ((end.tv_sec + end.tv_usec * 1e-6) - (start.tv_sec + start.tv_usec * 1e-6)), bytes_sent_total);
+
+    //free(segment_recv.packet.data);
+    //free(segment_send.packet.data);
 
 	return 0;
 }
