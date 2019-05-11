1338016e3373831478d7109bd6980bc0da812e32
EmersonAS <emersonls2008@hotmail.com>
2019-05-11 17:13:52 -0300

    versão funcional do servidor - tamanho variável de buffer, protocolo stop-and-wait, sem temporização

--- TP2/code/servidorFTP.c
+++ TP2/code/servidorFTP.c
@@ -8,18 +8,14 @@
 
 #include "tp_socket.h"
 
-#define FILE_NAME_SIZE 1024	// Tamanho máximo do buffer p/ troca de info da conexão e nome do arquivo
-
-
-typedef struct packet{
-    char data[20];
-} Packet;
-
+#define FILE_NAME_SIZE 1024	    // Tamanho máximo do buffer p/ troca de info da conexão e nome do arquivo
+#define OFFSET 10               // 
 
 typedef struct segment {
-    Packet packet;
     int seq_no;
-    int ack;
+    char *ack;
+    char *pkt_data;
+    int pkt_data_size;
 } Segment;
 
 
@@ -29,12 +25,10 @@ int main(int argc, char const *argv[]) {
 	int server_socket;                     	    // Descritor de socket (equivalente a um HANDLE)
     so_addr client_addr;
 
-    int PORT = atoi(argv[1]);
-    int BUFFER_DATA_SIZE = atoi(argv[2]);
+    int port = atoi(argv[1]);
+    int tam_buffer = atoi(argv[2]);
 
     char file_Name[FILE_NAME_SIZE] = {0};	// buffer para...
-    
-    char buffer_Data[BUFFER_DATA_SIZE];    // EXCLUIR	// buffer para armazenar tempoariamente os bytes (conteúdo) das msgs a serem enviadas
 
     struct timeval start, end;    // Estruturas com variáveis tv_sec (tipo time_t) e tv_usec (tipo suseconds_t)
     gettimeofday(&start, NULL);   // Inicia a contagem de segundos e microsegundos desde 01/01/1970 00:00:00
@@ -44,7 +38,7 @@ int main(int argc, char const *argv[]) {
         exit(1);
     }
 
-    if ((server_socket = tp_socket((unsigned short) PORT)) < 0) {
+    if ((server_socket = tp_socket((unsigned short) port)) < 0) {
         printf("\ttp_socket falhou.\n");
         exit(1);
     }
@@ -67,51 +61,96 @@ int main(int argc, char const *argv[]) {
   	    printf("\tcould not open file.\n");
   	    exit(1);
   	}
-
     
     /* LÓGICA DO STOP-AND-WAIT */
 
     int segment_id = 0;
-    Segment segment_send;
-    Segment segment_recv;
+    Segment *segment_send = (Segment *) malloc(sizeof(Segment));
+    Segment *segment_recv = (Segment *) malloc(sizeof(Segment));
 
+    segment_send->pkt_data = (char *) malloc(tam_buffer * sizeof(char) + OFFSET + 1);
+    segment_send->ack = (char *) malloc(sizeof(char));
+
+    segment_recv->pkt_data = (char *) malloc(tam_buffer * sizeof(char) + OFFSET + 1);
+    segment_recv->ack = (char *) malloc(sizeof(char));
+
+    //int segment_size = sizeof(Segment) + (tam_buffer * sizeof(char) + OFFSET + 1);
+
+    //************************************************************************
+
+    char buffer[tam_buffer];
+
+    int data_to_read = 1;
     int ack_recv = 1;
     int wait_for_ack = 1;
 
-    while( (bytes_sent = fread(segment_send.packet.data, sizeof(char), sizeof(segment_send.packet.data)-1, File_read)) > 0 ) {
+    while (data_to_read) {
+
+        char tmp[OFFSET];
+        sprintf(tmp, "%d", segment_id);
+        strcat(segment_send->pkt_data, tmp);
+        strcat(segment_send->pkt_data, ":");
+
+        // printf("tmp: %s\n", tmp);                                                        // DELETE
+        // printf("segment_send->pkt_data (before read): %s\n", segment_send->pkt_data);    // DELETE
+
+        if ((bytes_sent = fread(buffer, sizeof(char), tam_buffer - 1, File_read)) <= 0) {
+            data_to_read = 0;
+            //wait_for_ack = 0;
+        }
+
+        buffer[((bytes_sent < tam_buffer)? bytes_sent: tam_buffer)] = '\0';           // DELETE
+
+        strcat(segment_send->pkt_data, buffer);
         
-        bytes_sent_total += bytes_sent;             // Atualiza total de bytes lido/enviado
+        printf("buffer: %s, bytes_read = %d\n", buffer, bytes_sent);                      // DELETE
+        printf("segment_send->pkt_data (after read): %s\n", segment_send->pkt_data);      // DELETE
+        
+        bytes_sent_total += bytes_sent;             // Atualiza total de bytes lidos
+        printf("%d bytes read\n", bytes_sent);
 
-        if (ack_recv == 1){
+        if (ack_recv == 1) {
 
-            segment_send.packet.data[((bytes_sent < 20)? bytes_sent : 20)] = '\0'; //BUFFER_DATA_SIZE = 20 bytes
-            segment_send.seq_no = segment_id;
-            segment_send.ack = 0;
+            //printf("segment_send->pkt_data: %s\n", segment_send->pkt_data);                     // DELETE
+            
+            //segment_send->seq_no = segment_id;                                                // DELETE
+            //segment_send->ack = 0;                                                            // DELETE
 
-            tp_sendto(server_socket, (char *) &segment_send, sizeof(Segment), &client_addr);
-            wait_for_ack = 1;
+            segment_send->pkt_data_size = strlen(segment_send->pkt_data);
+            
+            //printf("strlen(segment_send->pkt_data): %lu\n", strlen(segment_send->pkt_data));  // DELETE
+
+            tp_sendto(server_socket, segment_send->pkt_data, segment_send->pkt_data_size, &client_addr);
+            
+            wait_for_ack = 1; // DELETE?????????????????
 
             printf("\tpkt sent\n");
 
         }
 
-        while(wait_for_ack == 1) {
+        if (data_to_read == 0) {
+            wait_for_ack = 0;
+        }
 
-            status = tp_recvfrom(server_socket, (char *) &segment_recv, sizeof(Segment), &client_addr);
+        while(wait_for_ack) {
+
+            status = tp_recvfrom(server_socket, segment_recv->ack, sizeof(segment_recv->ack), &client_addr);
+
+            printf("%d\n", atoi(segment_recv->ack));
 
             if (status > 0) {
-                if (segment_recv.seq_no == 0 && segment_recv.ack == segment_id + 1) {
-                    ack_recv = 1;
+                if (atoi(segment_recv->ack) == segment_id + 1) {
+                    //ack_recv = 1;                                         // DELETE
                     wait_for_ack = 0;
                     printf("\tACK received\n");
                 } else {
                     printf("\tACK not received\n");
-                    //ack_recv = 0;
+                    //ack_recv = 0;                                         // DELETE
                     wait_for_ack = 1;
                 }
             } else {
                 if (errno == EWOULDBLOCK) {
-                    tp_sendto(server_socket, (char *) &segment_send, sizeof(Segment), &client_addr);
+                    tp_sendto(server_socket, segment_send->pkt_data, segment_send->pkt_data_size, &client_addr);
                     printf("\ttimeout event\n");
                     printf("\tpkt resent\n");
                     wait_for_ack = 1;
@@ -122,11 +161,23 @@ int main(int argc, char const *argv[]) {
             }
 
         }
+        
+        memset(buffer, 0x0, strlen(buffer));
+        memset(segment_send->pkt_data, 0x0, strlen(segment_send->pkt_data));
+        
+        //printf("segment_send->pkt_data: %s\n", segment_send->pkt_data);       // DELETE
 
         segment_id++;
     }
     
     fclose(File_read);   // Fecha arquivo de leitura
+
+    free(segment_send->pkt_data);
+    free(segment_recv->pkt_data);
+    free(segment_send->ack);
+    free(segment_recv->ack);
+    free(segment_send);
+    free(segment_recv);
 
     gettimeofday(&end, NULL);      // Inicia a contagem de segundos e microsegundos desde 01/01/1970 00:00:00
 
@@ -150,7 +201,3 @@ int main(int argc, char const *argv[]) {
     /* Fim - Lógica do Temporizador*/
 
 
-    /* // Identificando um evento de timout
-
-    
-    */
